2b93050c789a09a3f522fa34f15ba802659a81fe
Hasar Ali <hasar.ali@protonmail.com>
2021-04-13 10:47:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function makeName(dogName, dogBreed) {
+function nameDog(dogName, dogBreed) {
   return `${dogName} the ${dogBreed}`;
+}
+
+function doWithDog(activity, name) {
+  const sentence = `${activity} ${name}`;
+  console.log(sentence);
+  return sentence;
+}
+
+function wakeDog(dogName, dogBreed) {
+  const str = `Wake ${nameDog(dogName, dogBreed)}`;
+  console.log(str);
+  return str;
 }
